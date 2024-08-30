6e319d1617c94c0cfa9e60b573b53113989ccb93
ujjwal <khannaujjwal21@gmail.com>
2024-08-31 04:58:25 +0530

    resume-backend

---
+++ strapi/types/generated/components.d.ts
@@ -0,0 +1,56 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface EducationEducation extends Schema.Component {
+  collectionName: 'components_education_educations';
+  info: {
+    displayName: 'Education';
+    icon: 'train';
+  };
+  attributes: {
+    universityName: Attribute.String;
+    degree: Attribute.String;
+    major: Attribute.String;
+    startDate: Attribute.String;
+    endDate: Attribute.String;
+    description: Attribute.Text;
+  };
+}
+
+export interface ExperienceExperience extends Schema.Component {
+  collectionName: 'components_experience_experiences';
+  info: {
+    displayName: 'Experience';
+    icon: 'bulletList';
+  };
+  attributes: {
+    title: Attribute.String;
+    companyName: Attribute.String;
+    city: Attribute.String;
+    state: Attribute.String;
+    startDate: Attribute.String;
+    endDate: Attribute.String;
+    workSummery: Attribute.Text;
+  };
+}
+
+export interface SkillsSkills extends Schema.Component {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+    icon: 'crown';
+  };
+  attributes: {
+    name: Attribute.String;
+    rating: Attribute.Integer;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'education.education': EducationEducation;
+      'experience.experience': ExperienceExperience;
+      'skills.skills': SkillsSkills;
+    }
+  }
+}
