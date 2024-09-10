b1e1d88f7fff2d1345c7d4b31b427e830ae8d665
ujjwal <khannaujjwal21@gmail.com>
2024-09-11 01:14:25 +0530

    yes

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 10000),
+  port: process.env.PORT || env.int('PORT', 10000),
   app: {
     keys: env.array('APP_KEYS'),
   },
